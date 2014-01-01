863d157632934132f3c2e662ebaa64a4ae484e74
chenchiyuan <chenchiyuan03@gmail.com>
2014-01-01 13:55:24 +0800

    增加酒店

--- hawaii/apps/hotel/views.py
+++ hawaii/apps/hotel/views.py
@@ -2,3 +2,13 @@
 # __author__ = chenchiyuan
 
 from __future__ import division, unicode_literals, print_function
+from django.views.generic import View
+from libs.http import json_response
+
+
+class HotelDetailView(View):
+    def dispatch(self, request, *args, **kwargs):
+        return super(HotelDetailView, self).dispatch(request, *args, **kwargs)
+
+    def get(self, requests, *args, **kwargs):
+        return json_response
